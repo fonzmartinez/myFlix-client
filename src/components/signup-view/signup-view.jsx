def994fd8b538991a38a8b5f1061e945bb0db030
fonzmartinez <119453742+fonzmartinez@users.noreply.github.com>
2023-02-17 21:33:02 -0600

    Added URL for signup

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -18,7 +18,7 @@ export const SignupView = () => {
       Birthday: birthday
     };
 
-    fetch("SIGNUP_URL", {
+    fetch("https://enigmatic-eyrie-99477.herokuapp.com/users", {
       method: "POST",
       body: JSON.stringify(data),
       headers: {
