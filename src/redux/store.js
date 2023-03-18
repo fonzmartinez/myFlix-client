52715be6fe6e03c09f1ed2e976a8afd1054cca14
fonzmartinez <119453742+fonzmartinez@users.noreply.github.com>
2023-03-18 00:39:03 -0500

    Added Redux

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import moviesReducer from "./reducers/movies";
+import userReducer from "./reducers/user";
+import tokenReduser from "./reducers/token"
+
+export const store = configureStore({
+  reducer: { movies: moviesReducer, user: userReducer, token: tokenReduser },
+});
